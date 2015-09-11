06fc7e0c45e6c766ac5d30459c8bab27deb933ca
babyshoes <dianecai@gmail.com>
2015-09-10 22:52:42 -0400

    all done

--- js/selectors.js
+++ js/selectors.js
@@ -8,3 +8,25 @@ $(document).ready(function(){
 });
 
 // declare your functions here...
+function h1Selector(){
+  return $('h1');
+}
+
+function liInOlSelector(){
+  return $('ol li');
+}
+
+function linkSelector(){
+  return $('.box5 a');
+}
+
+function imageSelector(){
+  return $('img[alt="cat sleeping"]')
+}
+
+function checkboxInputSelector(){
+  var checkboxes = $(':checkbox');
+  for(var i = 0; i<checkboxes.length; i++){
+    $(checkboxes[i]).wrap("<span class='pinky'></span>");
+  }
+}
